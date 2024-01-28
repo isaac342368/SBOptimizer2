0c22abecff113d5850992d49a7a5a90365bbbe92
Isaac <smartisaac34@gmail.com>
2024-01-28 17:08:54 -0600

    new

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 const repoName = 'SBOptimizer2';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SBOptimizer2/',
+  base: process.env.NODE_ENV === 'production' ? '/SBOptimizer2/' : './',
   plugins: [react()],
 })
